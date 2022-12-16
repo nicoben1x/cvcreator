9f0f3b8638604e856fd55bcf79ba0a32ac2215a8
nicoben1x <88980384+nicoben1x@users.noreply.github.com>
2022-12-16 20:08:58 -0300

    Se añadieron mas campos para llenar el CV

--- src/App.js
+++ src/App.js
@@ -5,9 +5,51 @@ function App() {
 
   const [nombre, setNombre] = useState('');
   const [dni, setDni] = useState('');
+  const [experienciaitem, setExperiencia] = useState([]);
 
   function addExperiencia() {
     document.querySelector(".exp-form").classList.remove("no-display");
+  }
+
+  function addEducacion() {
+    document.querySelector(".edu-form").classList.remove("no-display");
+  }
+
+  function addSkills() {
+    document.querySelector(".skills-form").classList.remove("no-display");
+  }
+
+  function addIdiomas() {
+    document.querySelector(".idiomas-form").classList.remove("no-display");
+  }
+
+  function preventThis2(e) {
+
+    // IMPORTANTE !! -- Hay que hacer una funcion por cada segmento, esta esta hecha para experiencia.
+
+
+    //Evita que se recarge toda la pagina web
+    e.preventDefault();
+
+    //Convierte los datos recibidos del form en un objeto,pero raro.
+    const myFormData = new FormData(e.target);
+    console.log(myFormData);
+
+    //Convierte ese objeto raro en un objeto normal.
+    const formDataObj = {};
+    myFormData.forEach((value, key) => (formDataObj[key] = value));
+    console.log(formDataObj);
+
+    //Añade el objeto a la lista de experiencias
+    const newArray = experienciaitem;
+    newArray.push(formDataObj)
+    setExperiencia(newArray);
+    console.log(experienciaitem);
+
+    //
+
+    document.querySelector(".exp-form").classList.add("no-display");
+
   }
 
   function preventThis(e) {
@@ -27,23 +69,6 @@ function App() {
 
   }
 
-  
-  function preventThis2(e) {
-    e.preventDefault();
-    
-    
-    //Convierte los datos recibidos del form en un objeto,pero raro.
-    const myFormData = new FormData(e.target);
-    console.log(myFormData);
-
-    //Convierte ese objeto raro en un objeto normal.
-    const formDataObj = {};
-    myFormData.forEach((value, key) => (formDataObj[key] = value));
-    console.log(formDataObj);
-
-  }
-
-
   return (
     <div className="App">
       <div className="principal-div">
@@ -52,8 +77,26 @@ function App() {
           <div className="div-test">
             <label htmlFor="nombre">Ingresa tu nombre:</label>
             <input required className="nombreClass" type="text" name="nombre" />
+            <label htmlFor="profesion">Ingresa tu profesion:</label>
+            <input required className="profesionClass" type="text" name="profesion" />
+            <label htmlFor="desc-per">Descripción:</label>
+            <input required className="desc-per" type="text" name="desc-per" />
+            <label htmlFor="tel-per">Telefono:</label>
+            <input required className="tel-per" type="text" name="tel-per" />
+            <label htmlFor="email-per">Email:</label>
+            <input required className="email-per" type="email" name="email-per" />
+            <label htmlFor="dir-per">Direccion:</label>
+            <input required className="dir-per" type="text" name="dir-per" />
+            <label htmlFor="edad-per">Edad:</label>
+            <input required className="edad-per" type="text" name="edad-per" />
+            
             <label htmlFor="dni">Ingresa tu DNI:</label>
             <input required className="dniClass" type="number" name="dni" />
+
+            <label htmlFor="imagen-per">Imagen:</label>
+            <input required className="imagen-per" type="text" name="imagen-per" />
+
+
             <input className="btn-enviar" type="submit" value="Enviar" />
 
 
@@ -67,7 +110,7 @@ function App() {
         <div className="experiencia">
           <label htmlFor="experienciafor">Ingresa tu Experiencia:</label>
           <button className="btn-enviar" onClick={addExperiencia}>Añadir</button>
-          <form onSubmit={preventThis2}className="exp-form no-display">
+          <form onSubmit={preventThis2} className="exp-form no-display">
             <label htmlFor="nombre-exp">Empresa:</label>
             <input required className="nombre-exp" type="text" name="nombre-exp" />
             <label htmlFor="empleo-exp">Empleo:</label>
@@ -78,6 +121,48 @@ function App() {
             <input required className="desc-exp" type="text" name="desc-exp" />
             <label htmlFor="imagen-exp">Imagen:</label>
             <input required className="imagen-exp" type="text" name="imagen-exp" />
+            <input className="btn-enviar" type="submit" value="Enviar" />
+          </form>
+        </div>
+
+        <div className="educacion">
+          <label htmlFor="educacionfor">Ingresa tu Educacion:</label>
+          <button className="btn-enviar" onClick={addEducacion}>Añadir</button>
+          <form onSubmit={preventThis2} className="edu-form no-display">
+            <label htmlFor="nombre-edu">Institucion:</label>
+            <input required className="nombre-edu" type="text" name="nombre-edu" />
+            <label htmlFor="titulo-edu">Titulo:</label>
+            <input required className="titulo-edu" type="text" name="titulo-edu" />
+            <label htmlFor="fecha-edu">Fecha:</label>
+            <input required className="fecha-edu" type="text" name="fecha-edu" />
+            <label htmlFor="desc-edu">Descripción:</label>
+            <input required className="desc-edu" type="text" name="desc-edu" />
+            <label htmlFor="imagen-exp">Imagen:</label>
+            <input required className="imagen-edu" type="text" name="imagen-edu" />
+            <input className="btn-enviar" type="submit" value="Enviar" />
+          </form>
+        </div>
+
+        <div className="skills">
+          <label htmlFor="skillsfor">Ingresa tu Habilidad:</label>
+          <button className="btn-enviar" onClick={addSkills}>Añadir</button>
+          <form onSubmit={preventThis2} className="skills-form no-display">
+            <label htmlFor="nombre-skills">Habilidad:</label>
+            <input required className="nombre-skills" type="text" name="nombre-skills" />
+            <label htmlFor="nivel-skills">Porcentaje:</label>
+            <input required className="nivel-skills" type="text" name="nivel-skills" />
+            <input className="btn-enviar" type="submit" value="Enviar" />
+          </form>
+        </div>
+
+        <div className="idiomas">
+          <label htmlFor="idiomasfor">Ingresa tu Idioma:</label>
+          <button className="btn-enviar" onClick={addIdiomas}>Añadir</button>
+          <form onSubmit={preventThis2} className="idiomas-form no-display">
+            <label htmlFor="nombre-idiomas">Idioma:</label>
+            <input required className="nombre-idiomas" type="text" name="nombre-idiomas" />
+            <label htmlFor="nivel-idiomas">Porcentaje:</label>
+            <input required className="nivel-idiomas" type="text" name="nivel-idiomas" />
             <input className="btn-enviar" type="submit" value="Enviar" />
           </form>
         </div>
